589752c7976b1e7c18fc6ae581df757863e82e88
Somtoo <chukwurahb@gmail.com>
2019-12-26 05:57:03 -0600

    Learning Yargs

--- app.js
+++ app.js
@@ -1,6 +1,51 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
-const message = getNotes();
 
-console.log(message);
-console.log(chalk.blue.inverse.bold('Success!'));
+yargs.command({
+    command: 'add',
+    describe:'Add a note',
+    builder:{
+        body:{
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        },
+        title:{
+            describe: 'Note title',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title:' + argv.title,'\n'+'Body:' + argv.body)
+    }
+});
+
+yargs.command({
+    command: 'remove',
+    describe:'Remove a note',
+    handler: function () {
+        console.log('Removing  a new note')
+    }
+});
+
+yargs.command({
+    command: 'list',
+    describe:'List all note',
+    handler: function () {
+        console.log('Listing all notes')
+    }
+});
+
+yargs.command({
+    command: 'read',
+    describe:'Read note',
+    handler: function () {
+        console.log('Reading notes')
+    }
+});
+
+yargs.parse();
+
+
